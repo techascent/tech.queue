7f0a87f998f50bf3cbb689e573b7bff656b2f712
Chris Nuernberger <chris@thinktopic.com>
2018-08-03 10:00:17 -0600

    Further work on resource limit.  Time to climb.

--- src/tech/queue/resource_limit.clj
+++ src/tech/queue/resource_limit.clj
@@ -13,7 +13,8 @@ above or equal to zero we assume that those resources are available.
 A resource map can contain many entries of keyword->integer.
   At manager creation time a similar map is provided indicating the initial state."
   (:require [com.stuartsierra.component :as c]
-            [clojure.core.async :as async])
+            [clojure.core.async :as async]
+            [taoensso.timbre :as log])
   (:import [oshi SystemInfo]))
 
 
@@ -90,14 +91,93 @@ A resource map can contain many entries of keyword->integer.
             resource-map)))
 
 
+(defn- store-resources
+  [boundary-map
+   current-resources
+   op]
+  (->> current-resources
+       (reduce (fn [boundary-map [map-key map-val]]
+                 (let [existing (get boundary-map map-key)]
+                   (assoc boundary-map map-key
+                          (if existing
+                            (op existing map-val)
+                            map-val))))
+               boundary-map)))
+
+
+(defn- store-min-resources
+  [min-resources current-resources]
+  (store-resources min-resources current-resources min))
+
+
+(defn- store-max-resources
+  [max-resources current-resources]
+  (store-resources max-resources current-resources max))
+
+
+(defn- check-resource-amounts!
+  [initial-resources resource-map]
+  (when-let [invalid-resources
+             (->> resource-map
+                  (filter (fn [[k v]]
+                            (when-not (or (contains? initial-resources k)
+                                          (> v (get initial-resources k))))))
+                  seq)]
+    (throw (ex-info "Invalid resources, either not specified initially or larger than initial amounts"
+                    {:initial-resources initial-resources
+                     :resource-request resource-map}))))
+
+
+(defn- resource-thread-loop
+  [current-resources* request-chan notify-chan]
+  (loop []
+    (let [continue? (try
+                      ;;Loop exits if this request chan closes.  Else it always recurs.
+                      (let [value (async/alt!!
+                                    request-chan ([result] result)
+                                    (async/timeout 50) :timeout
+                                    notify-chan :notify)]
+                        (cond
+                          (= value :timeout)
+                          true
+                          (= value :notify)
+                          true
+                          :else
+                          (when value
+                            (let [{:keys [resource-map result-chan]} value]
+                              ;;Inner loop exits when resources may be successfully allocated else
+                              ;;spins waiting for notification that resources were released.
+                              (loop [current-resources @current-resources*]
+                                (let [request-result (request-resource-map current-resources resource-map)]
+                                  (if (and request-result
+                                           (compare-and-set! current-resources* current-resources request-result))
+                                    (do
+                                      (async/close! result-chan)
+                                      true)
+                                    (do
+                                      (async/<!! notify-chan)
+                                      (recur @current-resources*)))))))))
+                      (catch Throwable e
+                        (log/error e)
+                        (Thread/sleep 2000)
+                        true))]
+      (when continue?
+        (recur))))
+  (log/warn "resource limiter thread exit")
+  (loop [item (async/<!! request-chan)]
+    (when item
+      (async/>!! (:result-chan item) (ex-info "resource limit thread exit" {}))
+      (recur (async/<!! request-chan)))))
+
+
 (defrecord ResourceManager [initial-resources request-chan notify-chan
                             current-resources*
-                            max-resources*
                             min-resources*]
   PResourceManager
   (request-resources! [mgr resource-map options]
     (when-not request-chan
       (throw (ex-info "Uninitialized" {})))
+    (check-resource-amounts! initial-resources resource-map)
     (let [result-chan (async/chan)]
       ;;The put request returns false when it fails.
       (when-let [result (async/>!! request-chan {:resource-map resource-map
@@ -108,8 +188,38 @@ A resource map can contain many entries of keyword->integer.
                             {:error error})))))))
 
   (release-resources! [mgr resource-map options]
-    (swap! min-released-cores min
-           (swap! active-cores #(- % core-count)))
-    (async/>!! notify-chan 1))
+    (loop [current-res @current-resources*]
+      (if (compare-and-set! current-resources* current-res (release-resource-map current-res resource-map))
+        (do
+          (swap! min-resources* store-min-resources current-res)
+          :ok)
+        (recur @current-resources*))))
 
-  )
+
+  c/Lifecycle
+  (start [this]
+    (if-not (:thread this)
+      (let [request-chan (async/chan)
+            notify-chan (async/chan)
+            current-res* (atom initial-resources)
+            thread (async/thread
+                     #(resource-thread-loop current-res* request-chan notify-chan))]
+        (assoc this
+               :request-chan request-chan
+               :notify-chan notify-chan
+               :current-resources* current-res*
+               :thread thread))
+      this))
+  (stop [this]
+    (if (:thread this)
+      (do
+        (async/close! request-chan)
+        (async/close! notify-chan)
+        ;;Wait for thread exit
+        (async/<!! (:thread this))
+        (dissoc this
+                :request-chan
+                :notify-chan
+                :active-cores
+                :thread))
+      this)))
