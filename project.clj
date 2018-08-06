3b3048edf37fa934aa532f85a654f105534c3c86
Chris Nuernberger <chris@thinktopic.com>
2018-08-06 11:42:38 -0600

    snap

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.queue "0.1.5"
+(defproject techascent/tech.queue "0.1.6-SNAPSHOT"
   :description "Queue abstraction with bindings at least to filesystem and amazon sqs."
   :url "http://github.com:tech-ascent/tech.queue"
   :license {:name "Eclipse Public License"
