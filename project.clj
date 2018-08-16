9926cee71bb5c544d0ff0d637f9f597af9b8c526
Chris Nuernberger <chris@thinktopic.com>
2018-08-16 10:28:35 -0600

    Release 0.1.12

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject techascent/tech.queue "0.1.12-SNAPSHOT"
+(defproject techascent/tech.queue "0.1.12"
   :description "Queue abstraction with bindings at least to filesystem and amazon sqs."
   :url "http://github.com:tech-ascent/tech.queue"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [techascent/tech.io "0.1.16"]
+                 [techascent/tech.io "0.1.17"]
                  [com.amazonaws/aws-java-sdk-sqs "1.11.341"]
                  [thinktopic/think.parallel "0.3.7"]
                  [factual/durable-queue "0.1.6" :exclusions [com.taoensso/nippy]]
